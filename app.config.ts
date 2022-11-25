1756c2a1db0086c3b264def5a05a4ab532248056
motea927 <motea927@yahoo.com.tw>
2022-11-26 00:36:20 +0800

    ADD: Add basic layout

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,29 @@
 export default defineAppConfig({
   docus: {
-    title: 'My Docs'
+    title: `Morty's blog`,
+    description: `This is Morty's tech blog, record and share a variety of coding information`,
+    url: 'http://morty.tw',
+    image: '/social-card-preview.png',
+    socials: {
+      github: 'motea927'
+    },
+    github: {
+      root: 'content',
+      edit: true,
+      contributors: false
+    },
+    aside: {
+      level: 1,
+      exclude: [],
+      collapsed: true
+    },
+    header: {
+      title: `Morty's blog`,
+      logo: false,
+      showLinkIcon: true
+    },
+    footer: {
+      credits: false
+    }
   }
 })
