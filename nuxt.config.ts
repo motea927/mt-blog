f642955833f0817b75a66d15bc2a8fbe014ceb8b
motea927 <motea927@yahoo.com.tw>
2022-11-26 14:45:00 +0800

    UPD: Use gtag in head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,26 @@
+const gaId = 'CVPQ8TCZXX'
+
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
+  extends: '@nuxt-themes/docus',
+  app: {
+    head: {
+      script: [
+        {
+          async: true,
+          src: `https://www.googletagmanager.com/gtag/js?id=${gaId}`
+        },
+        {
+          children: `
+            window.dataLayer = window.dataLayer || []
+            function gtag() {
+              dataLayer.push(arguments)
+            }
+            gtag('js', new Date())
+
+            gtag('config', '${gaId}')
+          `
+        }
+      ]
+    }
+  }
 })
